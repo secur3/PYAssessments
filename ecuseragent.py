4dd4ed1022adf23fe3b9bcc9076df9e892b47905
secur3 <ben+git@millerfam.me>
2020-04-15 11:04:49 -0500

    Minor
    Minor updates and bug fixes

--- ecuseragent.py
+++ ecuseragent.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 
-useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
