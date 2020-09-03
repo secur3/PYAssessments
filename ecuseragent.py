27fbb18379940528e7087d58e00dbc0bd165f90c
secur3 <ben+git@millerfam.me>
2020-09-03 17:15:38 -0500

    matadownloader.py
    Update to handle cookies allowing 100 results per page to be processed. Updated UA string

--- ecuseragent.py
+++ ecuseragent.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 
-useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
